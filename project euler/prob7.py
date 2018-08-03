79b9792521395acb0c16f63cf6908889a8544640
ManimaranN <manimaran.n36@gmail.com>
2018-08-03 21:41:26 +0530

    completed 7th problem successfuly

---
+++ project euler/prob7.py
@@ -0,0 +1,19 @@
+list = []
+test = 0
+list.append(2)
+num = 3
+while len(list) <= 10000:
+    for i in range(2,num-1):
+        if num % i == 0:
+            test = 1
+            break
+        else:
+            test = 0
+    if test == 0:
+        list.append(num)
+    num = num+1
+
+print(list)
+ln = len(list)
+print("largest prime number in the list of first 6 prime numbers is: ", list[ln-1])
+
