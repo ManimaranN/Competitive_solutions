6de82cf3d5dc35aea7c4e2911a1263653a790c6d
ManimaranN <manimaran.n36@gmail.com>
2018-08-03 20:52:12 +0530

    completd 6th problem

---
+++ project euler/prob6.py
@@ -0,0 +1,14 @@
+a = [num**2 for num in range(1,101) ]
+b = range(1,101)
+diff = sum(b)**2 - sum(a)
+print(diff) 
+
+# The sum of the squares of the first ten natural numbers is,
+
+# 12 + 22 + ... + 102 = 385
+# The square of the sum of the first ten natural numbers is,
+
+# (1 + 2 + ... + 10)2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
